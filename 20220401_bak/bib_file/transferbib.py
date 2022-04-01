ce435f8d25731a79ffefd88ac0ad07b7882e9bb2
eenju <lilab360@outlook.com>
2022-04-01 14:05:12 +0800

    move from desktop

---
+++ 20220401_bak/bib_file/transferbib.py
@@ -0,0 +1,87 @@
+#默认bib文件已按照时间顺序排列
+import bibtexparser
+from deal_bib import *
+import time
+import re
+from tkinter import messagebox
+
+start=time.localtime().tm_year
+end=2016
+
+def readbib(bibpath='./lilab.bib'):
+    '''读取bib文件，并将month格式化'''
+    #读取文件
+    with open(bibpath,'r+',encoding='utf8') as f:
+        tmp=f.read()
+    #bibtex文件的month格式不加{}，导致报错
+    #month    = may,变为month    = {may},
+    patt_month=r'month(\s*?)=(\s*?)([a-z]{3})'
+    tmp=re.sub(patt_month,lambda m:'month'+m.group(1)+'='+m.group(2)+'{'+m.group(3)+'}',tmp)
+    biball=bibtexparser.loads(tmp)
+    return biball
+
+def dictbib(biball):
+    '''将bib的所有条目变为时间戳对应的字典'''
+    entry_dict={}
+    timestamp=[]
+    for item in biball.entries:
+        _tmp=int(item['note'])
+        timestamp.append(_tmp)
+        entry_dict.update({_tmp:item})
+    set_time=set(timestamp)
+    if len(set_time)==len(timestamp):
+        timestamp.sort(reverse=True)
+        return timestamp,entry_dict
+    else:
+        return None,None
+
+def main():
+    '''主函数，生成HTML'''
+    category='<tr><th class="pubyear">yearcategory</th></tr>'
+    html='<table class="table">\n'
+    htmlyear=0
+    stop=False
+    biball=readbib()
+    timestamp,entry_dict=dictbib(biball)
+    if timestamp==None:
+        messagebox.showerror('note域重复，请检查')
+    else:
+        for item in timestamp:
+            bib=entry_dict[item]
+            year=int(bib['year'])
+            if year>end-1 and year!=htmlyear:
+                newcategory=category.replace('yearcategory',bib['year'])+'\n\n'
+                html=html+newcategory
+                htmlyear=year
+            elif not stop and year<=end-1:
+                tmp=str(end)+'年之前'
+                newcategory=category.replace('yearcategory',tmp)+'\n\n'
+                html=html+newcategory
+                stop=True
+            else:
+                pass
+            newitem=formatitem(bib)
+            html=html+newitem+'\n\n'
+        html=html+'</table>'
+        with open('./pypublist.html','w+',encoding='utf8') as f:
+            f.write(html)
+
+if __name__=='__main__':
+    main()
+
+#旧版所用，新版不再需要
+# monlist=['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+'''
+#旧版
+for i in range(start,end-1,-1):
+    years=str(i)
+    newcategory=category.replace('yearcategory',years)+'\n'
+    html=html+newcategory
+    for bib in biball.entries:
+        if bib['year']==years:
+            newitem=formatitem(bib)
+            html=html+newitem+'\n\n'
+html=html+'</table>'
+with open('./pypublist.html','w+',encoding='utf8') as f:
+    f.write(html)
+'''
---
+++ bib_file/deal_bib.py
@@ -0,0 +1,88 @@
+import bibtexparser
+import re
+pattern1=re.compile(r'\$_{(.+?)}\$')
+pattern2=re.compile(r'\$\^{(.+?)}\$')
+html_escape_table = {
+    '&': '&amp;',
+    '--':'–',
+    '"': '&quot;',
+    "'": '&apos;',
+    '\\alpha':'α','\\beta':'β','\\gamma':'γ','\\delta':'δ','\\epsilon':'ϵ','\\varepsilon':'ε',
+    '\\zeta':'ζ','\\eta':'η','\\theta':'θ','\\iota':'ι','\\kappa':'κ','\\lambda':'λ','\\mu':'μ',
+    '\\nu':'ν','\\xi':'ξ','\\omicron':'ο','\\pi':'π','\\rho':'ρ','\\sigma':'σ','\\tau':'τ',
+    '\\upsilon':'υ','\\phi':'ϕ','\\varphi':'φ','\\chi':'χ','\\psi':'ψ',
+    '{':'','}':'',
+    }
+paper='<tr><td class="paperlist"><a href="doiurl" target="_blank">papertitle</a><br/>paperauthor<br/>source</b><button data-toggle="collapse" data-target="#bibkey" type="button" class="mybtn btn-link">bib</button><div id="bibkey" class="collapse bibtex">bibinfo</div>  </td></tr>'
+
+
+def change(text):
+    '''替换上下标'''
+    tmp=re.sub(pattern1,lambda m:'<sub>'+m.group(1)+'</sub>',text)
+    tmp=re.sub(pattern2,lambda m:'<sup>'+m.group(1)+'</sup>',tmp)
+    for item in html_escape_table:
+        tmp=tmp.replace(item,html_escape_table[item])
+    return tmp
+
+def gettitle(bib):
+    '''获取标题'''
+    title=change(bib['title'])
+    #title=[html_escape_table.get(c,c) for c in title]
+    title=''.join(title)
+    return title
+
+def getauthor(bib):
+    '''获取作者'''
+    author=bib['author'].split(' and ')
+    for i in range(len(author)):
+        tmp=author[i]
+        tmp=tmp.split(', ')
+        author[i]=tmp[1]+' '+tmp[0]
+    author='Author(s): '+', '.join(author)
+    return author
+
+def getsource(bib):
+    '''获取出版信息'''
+    if bib['ENTRYTYPE'].lower()=='article':
+        #文献类型为article时
+        pages=bib['pages'].replace('--','–')
+        try:
+            number=' ('+bib['number']+')'
+        except:
+            number=''
+        source='Source: <b><i>'+bib['journal']+'</i>, '+bib['volume']+number+', '+pages+', '+bib['year']+'.</b>'
+    elif bib['ENTRYTYPE'].lower()=='inproceedings':
+        #文献类型为inproceedings时
+        source='Source: <b><i>'+bib['booktitle']+'</i>, '+bib['year']+'.</b>'
+    elif bib['ENTRYTYPE'].lower()=='misc':
+        #文献类型为misc时，这里用misc指代已接收无DOI文章
+        source='Source: <b><i>'+bib['howpublished']+'</i>, '+bib['year']+', accepted.</b>'
+    return source+'&nbsp;'
+
+def getbibinfo(bib):
+    '''不区分文献类型'''
+    bibinfo='@'+bib['ENTRYTYPE']+'{'+bib['ID']+',</br>\n'
+    for item in bib:
+        if item !='ENTRYTYPE' and item !='ID' and item !='note':
+            bibinfo=bibinfo+item+'={'+bib[item]+'},</br>\n'
+    return bibinfo+'}'
+
+def getdoiurl(bib):
+    if bib['ENTRYTYPE'].lower()=='misc':
+        #文献类型为misc时，这里用misc指代已接收无DOI文章
+        return '#'
+    else:
+        return 'https://doi.org/'+bib['doi']
+    
+#paper='<tr><td class="paperlist"><a href="doiurl" target="_blank">papertitle</a><br/>paperauthor<br/>source</b><button data-toggle="collapse" data-target="#bibkey" type="button" class="mybtn btn-link">bib</button><div id="bibkey" class="collapse bibtex">bibinfo</div>  </td></tr>'
+def formatitem(bib):
+    '''获取格式化输出'''
+    bibkey=bib['ID']
+    tmp=paper.replace('doiurl',getdoiurl(bib))
+    tmp=tmp.replace('papertitle',gettitle(bib))
+    tmp=tmp.replace('paperauthor',getauthor(bib))
+    tmp=tmp.replace('source',getsource(bib))
+    tmp=tmp.replace('bibkey',bibkey)
+    tmp=tmp.replace('bibinfo',getbibinfo(bib))
+    return tmp
+    

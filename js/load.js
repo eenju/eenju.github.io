20204fb7c020a1d575bdefbf60523acbb1371e8a
eenju <101937795+eenju@users.noreply.github.com>
2022-11-18 17:37:21 +0800

    Add files via upload

--- js/load.js
+++ js/load.js
@@ -1,8 +1,10 @@
-        $(document).ready(function(){
-            $("#header0").load("./reuse/header.htm");   
-            $("#myindicators").load("../reuse/slide_indicators.htm");
-            $("#myinner").load("../reuse/slide_inner.htm");
-           $("#publist").load("./reuse/publist.htm");
-            $("#footer0").load("./reuse/footer.htm");  
-            $.get("./reuse/gotop.htm",function(data){$("body").append(data);});   
-        });
+$(document).ready(function(){
+    $("#header0").load("./reuse/header.htm");   
+    $("#header1").load("./reuse/header_en.htm");   
+    $("#myindicators").load("./reuse/slide_indicators.htm");
+    $("#myinner").load("./reuse/slide_inner.htm");
+    $("#publist").load("./reuse/publist.htm");
+    $("#footer0").load("./reuse/footer.htm");  
+    $("#footer1").load("./reuse/footer_en.htm");  
+    $.get("./reuse/gotop.htm",function(data){$("body").append(data);});   
+ });
